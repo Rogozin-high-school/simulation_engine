a30bd7fac1c131c55ba82096aa44d6a7384ea0a2
Yotam Salmon <yotam.salmon@gmail.com>
2018-01-23 14:02:20 +0200

    Create Data utilities

---
+++ Simulation Engine/Assets/Script/DataUtil.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+
+public static class DataUtil {
+
+	/*
+	Converts an integer to 4 bytes.
+	*/
+	public static byte[] ItB(int input)
+	{
+		return BitConverter.GetBytes(input);
+	}
+
+	/*
+	Converts 4 bytes array to an integer value.
+	*/
+	public static int BtI(byte[] input) 
+	{
+		return BitConverter.ToInt32(input, 0);
+	}
+}
