d7dc863a62fea56f2881d9203866a57805538747
Yotam Salmon <yotam.salmon@gmail.com>
2018-01-29 18:22:07 +0200

    Create magnetic visualization system

---
+++ Simulation Engine/Assets/Script/MagneticFieldManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagneticFieldManager : MonoBehaviour {
+
+
+	public static MagneticFieldManager instance;
+
+	[SerializeField]
+	private Vector3 field;
+
+	public Vector3 Field {
+		get {
+			return field;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static Vector3 GetField()
+	{
+		return instance == null ? Vector3.zero : instance.field;
+	}
+}
