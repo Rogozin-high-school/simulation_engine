a434d129b88b473c62b6cce58bdc6eab56232a8d
Yotam Salmon <yotam.salmon@gmail.com>
2018-01-30 13:55:03 +0200

    Create camera rotation menu

---
+++ Simulation Engine/Assets/CameraView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour {
+	
+	public float hdg = 0;
+	public float pitch = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.eulerAngles = new Vector3(pitch, hdg, 0);
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			hdg += Time.deltaTime * 30;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			hdg -= Time.deltaTime * 30;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			pitch += Time.deltaTime * 30;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			pitch -= Time.deltaTime * 30;
+		}
+	}
+}
---
+++ Simulation Engine/Assets/VisualManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualManager : MonoBehaviour {
+
+	LineRenderer magneticRenderer;
+
+	// Use this for initialization
+	void Start () {
+		magneticRenderer = gameObject.AddComponent<LineRenderer>();
+		magneticRenderer.widthCurve = new AnimationCurve(
+			new Keyframe(0, 0.1f),
+			new Keyframe(0.9f, 0.1f),
+			new Keyframe(0.9001f, 0.5f),
+			new Keyframe(1f, 0f)
+		);
+		magneticRenderer.material = new Material(Shader.Find("GUI/Text Shader"));
+		magneticRenderer.material.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		magneticRenderer.numPositions = 4;
+		magneticRenderer.SetPosition(0, transform.position);
+		magneticRenderer.SetPosition(1, transform.position + 0.9f * MagneticFieldManager.GetField());
+		magneticRenderer.SetPosition(2, transform.position + 0.9001f * MagneticFieldManager.GetField());
+		magneticRenderer.SetPosition(3, transform.position + MagneticFieldManager.GetField());
+	}
+}
